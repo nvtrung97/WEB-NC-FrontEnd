3cf9ce8936b62b6ab439540b0df98a8aa8740f26
ntngon8399 <goldbullet1412@gmail.com>
2021-08-02 01:47:27 +0700

    Updates

---
+++ precache-manifest.e67fb7e16f51581384e3690ffe3fdb2a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017bd7b9fcb7e9f20bdee94a40c6d624",
+    "url": "/react-online-course/index.html"
+  },
+  {
+    "revision": "b5deefb70b312c76afa1",
+    "url": "/react-online-course/static/css/2.bb4864b8.chunk.css"
+  },
+  {
+    "revision": "c9a5c98c53359751c3df",
+    "url": "/react-online-course/static/css/main.77d52777.chunk.css"
+  },
+  {
+    "revision": "b5deefb70b312c76afa1",
+    "url": "/react-online-course/static/js/2.f124a3bb.chunk.js"
+  },
+  {
+    "revision": "e778c996c5588efacd392c17ee5afdf3",
+    "url": "/react-online-course/static/js/2.f124a3bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a5c98c53359751c3df",
+    "url": "/react-online-course/static/js/main.4104a8e8.chunk.js"
+  },
+  {
+    "revision": "572243c43c3c7e8fe286",
+    "url": "/react-online-course/static/js/runtime-main.89978ef1.js"
+  },
+  {
+    "revision": "4ff6c7b7c0655c46361851643a1aa716",
+    "url": "/react-online-course/static/media/camp-k-12-logo.4ff6c7b7.svg"
+  },
+  {
+    "revision": "15b2a5c835fd85d194e6e6edc0281128",
+    "url": "/react-online-course/static/media/community.15b2a5c8.svg"
+  },
+  {
+    "revision": "9aa55bf5b09800787a26b1a1905c2a96",
+    "url": "/react-online-course/static/media/group-40.9aa55bf5.svg"
+  },
+  {
+    "revision": "af3da035ca01c02dc4024f4023a9cd9f",
+    "url": "/react-online-course/static/media/growth.af3da035.svg"
+  },
+  {
+    "revision": "fd6f5eee232e433e950f113a2f5dc1be",
+    "url": "/react-online-course/static/media/header-background.fd6f5eee.svg"
+  },
+  {
+    "revision": "4cc25fa34d3882fdd41be138e6d3235f",
+    "url": "/react-online-course/static/media/home.4cc25fa3.svg"
+  },
+  {
+    "revision": "a51f3e4adaf85cc5a3b2c3db10025b53",
+    "url": "/react-online-course/static/media/leaderboard.a51f3e4a.svg"
+  },
+  {
+    "revision": "5d5c5d80779eb1414c551548dfaef52e",
+    "url": "/react-online-course/static/media/medal.5d5c5d80.svg"
+  },
+  {
+    "revision": "9a39f5536d9d9b9a2a2d8d86841f91f7",
+    "url": "/react-online-course/static/media/menu.9a39f553.svg"
+  },
+  {
+    "revision": "b9ea7a575a5f1d449e86ee4970de8286",
+    "url": "/react-online-course/static/media/offer.b9ea7a57.svg"
+  },
+  {
+    "revision": "6272efe024a65f0919df6873f5de2acc",
+    "url": "/react-online-course/static/media/review.6272efe0.svg"
+  },
+  {
+    "revision": "39884cb87e1936475044bb1899964184",
+    "url": "/react-online-course/static/media/rupees.39884cb8.svg"
+  },
+  {
+    "revision": "cd465efd2b76e58728a2bb10c1b47659",
+    "url": "/react-online-course/static/media/tablet.cd465efd.svg"
+  },
+  {
+    "revision": "2ef608dc8307d93b3a040108562bdb4a",
+    "url": "/react-online-course/static/media/timer.2ef608dc.svg"
+  },
+  {
+    "revision": "130715e4f828f5292d2328cb36c74404",
+    "url": "/react-online-course/static/media/vision.130715e4.svg"
+  },
+  {
+    "revision": "42962023a915b05fae74dec3c3ea12b5",
+    "url": "/react-online-course/static/media/writer.42962023.svg"
+  }
+]);
